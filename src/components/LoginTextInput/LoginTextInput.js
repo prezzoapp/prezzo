617df1a71cb4ad3b412cd41a5560f1c438f10c52
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2018-05-31 23:56:09 -0700

    toggle password visibility option

--- src/components/LoginTextInput/LoginTextInput.js
+++ src/components/LoginTextInput/LoginTextInput.js
@@ -1,6 +1,7 @@
 // @flow
-import React from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
 import {FONT_FAMILY, FONT_FAMILY_MEDIUM} from '../../services/constants';
 
 type Props = {
@@ -9,18 +10,55 @@ type Props = {
   type: 'name' | 'email' | 'password'
 };
 
-const LoginTextInput = ({onChange, label, type}: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={type === 'password'}
-        onChangeText={text => onChange && onChange(text)}
-      />
-    </View>
-  );
+type State = {
+  isShowingPassword: boolean
 };
+
+class LoginTextInput extends Component<Props, State> {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
+  };
+
+  state = {
+    isShowingPassword: false
+  };
+
+  togglePasswordVisibility() {
+    const isShowingPassword = !this.state.isShowingPassword;
+    this.setState({isShowingPassword});
+  }
+
+  render() {
+    const {onChange, label, type} = this.props;
+    const {isShowingPassword} = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.labelContainer}>
+          <Text style={styles.label}>{label}</Text>
+
+          {
+            type === 'password' && (
+              <TouchableOpacity onPress={() => this.togglePasswordVisibility()}>
+                <Text style={styles.togglePasswordVisibility}>
+                  {isShowingPassword ? 'Hide' : 'Show'}
+                </Text>
+              </TouchableOpacity>
+            )
+          }
+        </View>
+
+        <TextInput
+          style={styles.input}
+          secureTextEntry={type === 'password' && !isShowingPassword}
+          onChangeText={text => onChange && onChange(text)}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -31,7 +69,21 @@ const styles = StyleSheet.create({
     borderBottomColor: '#0DD24A',
     backgroundColor: 'transparent'
   },
+  labelContainer: {
+    width: '100%',
+    height: 'auto',
+    flexDirection: 'row',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
   label: {
+    color: '#959595',
+    fontSize: 14,
+    fontWeight: '600',
+    fontFamily: FONT_FAMILY,
+    paddingBottom: 10
+  },
+  togglePasswordVisibility: {
     color: '#959595',
     fontSize: 14,
     fontWeight: '600',
