3159ebe039cf0d6a9914c9c64b2f51ae44194636
danstepanov <danhstepanov@gmail.com>
2018-06-12 19:41:04 -0700

    [WIP] connect Edit Profile components to redux store

--- src/containers/Profile/EditProfile/EditProfile.js
+++ src/containers/Profile/EditProfile/EditProfile.js
@@ -1,17 +1,52 @@
 // @flow
 import React, {Component} from 'react';
-import {Image, Keyboard, KeyboardAvoidingView, Text, TouchableOpacity, TouchableWithoutFeedback, View, StyleSheet} from 'react-native';
-import PropTypes from 'prop-types';
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import ImagePicker from 'react-native-image-picker';
+import {
+  updateAvatarURL,
+  updateFirstName,
+  updateLastName,
+  updatePhone,
+  updateAddress,
+  updateZip,
+  updateCity,
+  updateProfile
+} from '../../modules/signup';
 import ProfileDataField from '../../../components/ProfileDataField';
 import ProfileTextInput from '../../../components/ProfileTextInput';
 import {FONT_FAMILY, FONT_FAMILY_MEDIUM} from '../../../services/constants';
 
 const prezzoBlack = '#2B2C2C';
+const prezzoGreen = '#39B86C';
 
 type Props = {
-  updateFirstName: PropTypes.func.isRequired,
-  updateLastName: PropTypes.func.isRequired,
+  avatarURL: string,
+  firstName: string,
+  lastName: string,
+  phone: string,
+  address: string,
+  zip: string,
+  city: string,
+  updateavatarURL: Function,
+  updateFirstName: Function,
+  updateLastName: Function,
+  updatePhone: Function,
+  updateAddress: Function,
+  updateZip: Function,
+  updateCity: Function,
+  updateProfile: Function
 };
 
 type State = {
@@ -48,10 +83,35 @@ class EditProfile extends Component<Props, State> {
 
   toggleEditing() {
     if (this.state.isEditing) {
-      // TODO: Attempt to save all data to server for user and return new user object
+      // Save all data to server for user and return new user object
+      this.setState({isBusy: true});
+
+      const {avatarURL, firstName, lastName, phone, address, zip, city} = this.props;
+      this.props.updateProfile(avatarURL, firstName, lastName, phone, address, zip, city)
+        .then(() => this.navigateToUserProfile())
+        .catch(() => console.log('Something went wrong'))
+        .finally(() => this.setState({isBusy: false}));
     } else {
       this.setState({isEditing: true});
     }
+  }
+
+  navigateToUserProfile() {
+    this.props.navigation.goBack(null);
+  }
+
+  showAvatarActionSheet() {
+    const options = {title: 'Select an avatar'};
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image upload');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        console.log('Image URI: ', response.uri);
+        this.props.updateAvatarURL(response.uri);
+      }
+    });
   }
 
   render() {
@@ -61,7 +121,12 @@ class EditProfile extends Component<Props, State> {
           <View style={styles.container}>
             <View style={styles.headerContainer}>
               <View style={styles.avatarContainer}>
-                <Image style={styles.avatar} source={require('../../../../assets/images/etc/default-avatar.png')}/>
+                {this.state.isEditing
+                  ? <TouchableOpacity onPress={() => this.showAvatarActionSheet()}>
+                      <Image style={styles.avatar} source={require('../../../../assets/images/etc/default-avatar.png')} />
+                    </TouchableOpacity>
+                  : <Image style={styles.avatar} source={require('../../../../assets/images/etc/default-avatar.png')} />
+                }
               </View>
               <TouchableOpacity onPress={() => this.toggleEditing()}>
                 <View>
@@ -75,31 +140,37 @@ class EditProfile extends Component<Props, State> {
                     type='name'
                     label='First Name'
                     placeholder='Andreas'
+                    onChange={value => this.props.updateFirstName(value)}
                   />
                   <ProfileTextInput
                     type='name'
                     label='Last Name'
                     placeholder='Tamrin'
+                    onChange={value => this.props.updateLastName(value)}
                   />
                   <ProfileTextInput
                     type='number'
                     label='Phone'
                     placeholder='(477) 722-2796'
+                    onChange={value => this.props.updatePhone(value)}
                   />
                   <ProfileTextInput
                     type='name'
                     label='Address'
                     placeholder='215 Sage Alley'
+                    onChange={value => this.props.updateAddress(value)}
                   />
                   <ProfileTextInput
                     type='number'
                     label='Zip'
                     placeholder='12796'
+                    onChange={value => this.props.updateZip(value)}
                   />
                   <ProfileTextInput
                     type='name'
                     label='City'
                     placeholder='New York'
+                    onChange={value => this.props.updateCity(value)}
                   />
                 </View>
               : <View style={styles.bodyContainer}>
@@ -145,9 +216,12 @@ const styles = StyleSheet.create({
     marginTop: 90
   },
   edit: {
-    color: '#39B86C',
+    color: prezzoGreen,
     fontSize: 18,
     fontFamily: FONT_FAMILY
+  },
+  editAvatar: {
+    height: 28
   },
   headerContainer: {
     alignItems: 'center',
@@ -162,4 +236,23 @@ const styles = StyleSheet.create({
   }
 });
 
-export default EditProfile;
+export default connect(state => ({
+  avatarURL: state.get('profile').get('avatarURL'),
+  firstName: state.get('profile').get('firstName'),
+  lastName: state.get('profile').get('lastName'),
+  phone: state.get('profile').get('phone'),
+  address: state.get('profile').get('address'),
+  zip: state.get('profile').get('zip'),
+  city: state.get('profile').get('city')
+}), dispatch => {
+  return {
+    updateAvatarURL: bindActionCreators(updateAvatarURL, dispatch),
+    updateFirstName: bindActionCreators(updateFirstName, dispatch),
+    updateLastName: bindActionCreators(updateLastName, dispatch),
+    updatePhone: bindActionCreators(updatePhone, dispatch),
+    updateAddress: bindActionCreators(updateAddress, dispatch),
+    updateZip: bindActionCreators(updateZip, dispatch),
+    updateCity: bindActionCreators(updateCity, dispatch),
+    updateProfile: bindActionCreators(updateProfile, dispatch)
+  };
+})(EditProfile);
