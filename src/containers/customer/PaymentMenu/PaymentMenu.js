d8ec53bcefe7f68bd9c41eb47a990db5aa518ca0
Pankaj <pankaj@ocdlab.co>
2019-03-01 19:30:33 +0530

    Add check network validity & handle API(s) errors for Vendor Admin > Tables > Open, Queue, Completed tabs and all its internal components, remove unused code for these components/screen(s).

--- src/containers/customer/PaymentMenu/PaymentMenu.js
+++ src/containers/customer/PaymentMenu/PaymentMenu.js
@@ -20,15 +20,9 @@ import MenuButton from '../../../components/MenuButton';
 import { Feather } from '../../../components/VectorIcons';
 import EditableListItem from '../../../components/EditableListItem';
 import styles from './styles';
-import {
-  FONT_FAMILY_MEDIUM,
-  COLOR_WHITE,
-  INTERNET_NOT_CONNECTED,
-  NETWORK_REQUEST_FAILED,
-  TIME_OUT
-} from '../../../services/constants';
+import { FONT_FAMILY_MEDIUM, COLOR_WHITE } from '../../../services/constants';
 import LoadingComponent from '../../../components/LoadingComponent';
-import { showAlert } from '../../../services/commonFunctions';
+import { showAlertWithMessage } from '../../../services/commonFunctions';
 
 class PaymentMenu extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -77,13 +71,7 @@ class PaymentMenu extends Component {
       this.props.listCreditCards().then(() => {
           this.checkResponseMessage();
         })
-        .catch(err => {
-          if(err.message === NETWORK_REQUEST_FAILED) {
-            showAlert('Uh-oh!', INTERNET_NOT_CONNECTED, TIME_OUT);
-          } else {
-            showAlert('Uh-oh!', err.message, TIME_OUT);
-          }
-      });
+        .catch(err => showAlertWithMessage('Uh-oh!', err));
     });
   }
 
@@ -96,13 +84,7 @@ class PaymentMenu extends Component {
   removeCreditCard(id) {
     this.props.removeCreditCard(id)
     .then(() => {})
-    .catch(err => {
-      if(err.message === NETWORK_REQUEST_FAILED) {
-        showAlert('Uh-oh!', INTERNET_NOT_CONNECTED, TIME_OUT);
-      } else {
-        showAlert('Uh-oh!', err.message, TIME_OUT);
-      }
-    });
+    .catch(err => showAlertWithMessage('Uh-oh!', err));
   }
 
   removeCardAtIndex(id) {
