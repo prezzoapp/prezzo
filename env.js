abb2c38f5614b80d8ea23f1ee4c42d43d2637682
Pankaj Sood <pankaj@ocdlab.co>
2019-05-23 12:04:38 +0530

    Add functionality to show restaurant's menu item images in stack layout.

--- env.js
+++ env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  API_ROOT: 'https://prezzoapp.herokuapp.com'
+  // API_ROOT: 'https://prezzoapp.herokuapp.com'
+  API_ROOT: 'http://192.168.5.122:5000'
 };
--- src/components/RestaurantItem/RestaurantItem.js
+++ src/components/RestaurantItem/RestaurantItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground, Platform } from 'react-native';
 
 import PropTypes from 'prop-types';
 
-import { LinearGradient } from 'expo';
-
 import Swiper from 'react-native-swiper';
 
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import Carousel, { getInputRangeFromIndexes } from 'react-native-snap-carousel';
+
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 import styles from './styles';
 
@@ -16,7 +16,7 @@ import { Feather } from '../VectorIcons';
 
 import RatingBar from '../RatingBar';
 
-import CacheImage from '../CacheImage';
+import RestaurantItemImages from './RestaurantItemImages';
 
 import Button from '../Button';
 
@@ -26,107 +26,192 @@ import {
   FONT_FAMILY_MEDIUM
 } from '../../services/constants';
 
-const RestaurantItem = props => {
-  if(props.showText) {
-    return (
-      <View style={styles.item}>
-        <View style={styles.leftSideContainer}>
-          <Text style={styles.itemTitle}>
-            <Text numberOfLines={2}>{props.item.title}</Text>
-            <Text> - ${props.item.price}</Text>
-          </Text>
-          <Text style={styles.itemIngradients} numberOfLines={3}>
-            {props.item.description}
-          </Text>
-        </View>
-        <View style={styles.rightSideContainer}>
-          {(() => {
-            if(props.item.quantity === 0) {
-              return (
-                <TouchableOpacity
-                  activeOpacity={0.6}
-                  onPress={() =>
-                    props.addRemoveItemQuantity(props.item._id, 'add')
-                  }
-                >
-                  <Feather name="plus" size={22} color="white" />
-                </TouchableOpacity>
-              );
-            }
-            return (
-              <View style={{
-                  flexDirection: 'row',
-                  alignItems: 'center'
-                }}
-              >
-                <TouchableOpacity
-                  activeOpacity={0.6}
-                  onPress={() =>
-                    props.addRemoveItemQuantity(props.item._id, 'remove')
-                  }
-                >
-                  <Feather name="minus" size={22} color="green" />
-                </TouchableOpacity>
-
-                <Text
-                  style={[
-                    styles.itemTitle,
-                    { fontFamily: SF_PRO_TEXT_REGULAR }
-                  ]}
-                >
-                  {props.item.quantity}
-                </Text>
-
-                <TouchableOpacity
-                  activeOpacity={0.6}
-                  onPress={() =>
-                    props.addRemoveItemQuantity(props.item._id, 'add')
-                  }
-                >
-                  <Feather name="plus" size={22} color="green" />
-                </TouchableOpacity>
-              </View>
-            );
-          })()}
-        </View>
-      </View>
-    )
+export default class RestaurantItem extends Component {
+  shouldComponentUpdate(nextProps) {
+    if(
+      nextProps.item.quantity !== this.props.item.quantity ||
+      nextProps.showText !== this.props.showText
+    ) return true;
+    return false;
   }
-  return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemTitleInPhotoMode}>
-        {props.item.title} - ${props.item.price}
-      </Text>
 
-      <Swiper
-        style={styles.swiper}
-        loadMinimal
-        loop={false}
-        showsPagination
-        paginationStyle={styles.pagination}
-        activeDotColor='#0DD24A'
-        dotColor='#808080'
-      >
-        {props.item.imageURLs && props.item.imageURLs.map(image => (
-          <CacheImage
-              key={image}
-              source={image}
-              type='backgroundImage'
-              style={styles.itemImage}
-            >
-              <LinearGradient
-                colors={['transparent', '#1E1E1E']}
-                locations={[0, 0.95]}
-                style={styles.itemImageLinearGradient}
-              />
-          </CacheImage>
-        ))}
-      </Swiper>
+  renderItem = data => <RestaurantItemImages item={data.item} />;
 
-      <View style={styles.bottomContentHolder}>
-        <View>
-          {(() => {
-            if(props.item.quantity === 0) {
+  // CUSTOM STACK INTERPOLATION
+  stackScrollInterpolator (index, carouselProps) {
+      const range = [3, 2, 1, 0, -1];
+      const inputRange = getInputRangeFromIndexes(range, index, carouselProps);
+      const outputRange = range;
+
+      return { inputRange, outputRange };
+  }
+  // CUSTOM STACK ANIMATED STYLES
+  stackAnimatedStyles (index, animatedValue, carouselProps, cardOffset) {
+      const sizeRef = carouselProps.vertical ? carouselProps.itemHeight : carouselProps.itemWidth;
+      const translateProp = carouselProps.vertical ? 'translateY' : 'translateX';
+
+      const card1Scale = 0.9;
+      const card2Scale = 0.8;
+
+      cardOffset = !cardOffset && cardOffset !== 0 ? 18 : cardOffset;
+
+      const getTranslateFromScale = (cardIndex, scale) => {
+          const centerFactor = 1 / scale * cardIndex;
+          const centeredPosition = -Math.round(sizeRef * centerFactor);
+          const edgeAlignment = Math.round((sizeRef - (sizeRef * scale)) / 2);
+          const offset = Math.round(cardOffset * Math.abs(cardIndex) / scale);
+          return centeredPosition + edgeAlignment + offset;
+      };
+
+      return {
+          ...Platform.select({
+           ios: {
+             zIndex: carouselProps.data.length - index
+           }, android: {
+             elevation: carouselProps.data.length - index
+           }
+          }),
+          opacity: animatedValue.interpolate({
+              inputRange: [0, 1, 2, 3],
+              outputRange: [1, 0.75, 0.5, 0],
+              extrapolate: 'clamp'
+          }),
+          transform: [{
+              scale: animatedValue.interpolate({
+                  inputRange: [-1, 0, 1, 2],
+                  outputRange: [card1Scale, 1, card1Scale, card2Scale],
+                  extrapolate: 'clamp'
+              })
+          }, {
+              [translateProp]: animatedValue.interpolate({
+                  inputRange: [-1, 0, 1, 2, 3],
+                  outputRange: [
+                      -sizeRef * 0.5,
+                      0,
+                      getTranslateFromScale(1, card1Scale),
+                      getTranslateFromScale(2, card2Scale),
+                      getTranslateFromScale(3, card2Scale)
+                  ],
+                  extrapolate: 'clamp'
+              })
+          }]
+      };
+  }
+
+  render() {
+    console.log('Restaurant Item render called!');
+    if(this.props.showText) {
+      return (
+        <View style={styles.item}>
+          <View style={styles.leftSideContainer}>
+            <Text style={styles.itemTitle}>
+              <Text numberOfLines={2}>{this.props.item.title}</Text>
+              <Text> - ${this.props.item.price}</Text>
+            </Text>
+            <Text style={styles.itemIngradients} numberOfLines={3}>
+              {this.props.item.description}
+            </Text>
+          </View>
+          <View style={styles.rightSideContainer}>
+            {(() => {
+              if(this.props.item.quantity === 0) {
+                return (
+                  <TouchableOpacity
+                    activeOpacity={0.6}
+                    onPress={() =>
+                      this.props.addRemoveItemQuantity(
+                        this.props.item._id,
+                        'add'
+                      )
+                    }
+                  >
+                    <Feather name="plus" size={22} color="white" />
+                  </TouchableOpacity>
+                );
+              }
+              return (
+                <View style={{
+                    flexDirection: 'row',
+                    alignItems: 'center'
+                }}>
+                  <TouchableOpacity
+                    activeOpacity={0.6}
+                    onPress={() =>
+                      this.props.addRemoveItemQuantity(
+                        this.props.item._id,
+                        'remove'
+                      )
+                    }
+                  >
+                    <Feather name="minus" size={22} color="green" />
+                  </TouchableOpacity>
+
+                  <Text
+                    style={[
+                      styles.itemTitle,
+                      { fontFamily: SF_PRO_TEXT_REGULAR }
+                    ]}
+                  >
+                    {this.props.item.quantity}
+                  </Text>
+
+                  <TouchableOpacity
+                    activeOpacity={0.6}
+                    onPress={() =>
+                      this.props.addRemoveItemQuantity(
+                        this.props.item._id,
+                        'add'
+                      )
+                    }
+                  >
+                    <Feather name="plus" size={22} color="green" />
+                  </TouchableOpacity>
+                </View>
+              );
+            })()}
+          </View>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemTitleInPhotoMode}>
+          {this.props.item.title} - ${this.props.item.price}
+        </Text>
+
+        <Carousel
+          layout='stack'
+          layoutCardOffset={18}
+          data={this.props.item.imageURLs}
+          renderItem={this.renderItem}
+          sliderHeight={hp('44.55%')}
+          sliderWidth={wp('98%')}
+          itemHeight={hp('44.55%')}
+          itemWidth={wp('84%')}
+          scrollInterpolator={this.stackScrollInterpolator}
+          slideInterpolatedStyle={this.stackAnimatedStyles}
+          containerCustomStyle={{ left: -wp('7%') }}
+        />
+
+        <View style={styles.bottomContentHolder}>
+          <View>
+            {(() => {
+              if(this.props.item.quantity === 0) {
+                return (
+                  <Button
+                    style={itemOrderBtnStyles.commonBtn}
+                    textStyle={itemOrderBtnStyles.commonBtnText}
+                    onPress={() =>
+                      this.props.addRemoveItemQuantity(
+                        this.props.item._id,
+                        'add'
+                      )
+                    }
+                  >
+                    Order
+                  </Button>
+                );
+              }
               return (
                 <Button
                   style={itemOrderBtnStyles.commonBtn}
